df2c529ec7b8f5a1e62932f9f0507b7d1d5f0a92
mbochene <mtbochenek@gmail.com>
2019-03-28 18:15:12 +0100

    Rozszerzenie klasy Game

--- src/server/event_handlers.py
+++ src/server/event_handlers.py
@@ -1,68 +1,70 @@
 from flask_socketio import SocketIO, send, emit
 from engine import GameState
 
-#game = GameState()
-
-#whoNow = 'X'
-#wonLocalBoards = []
-# nieuzywane latelyPlayed = 9
-
 class Game:
     def __init__(self):
-        self.gameState = GameState()
-        self.playableFields = [0,1,2,3,4,5,6,7,8,9]
+        self.prepareNewRound()
         self.playerSymbol = ["none",'X','O']
+        self.scoreTable = {
+            'X' : 0,
+            'O' : 0
+        }
+
+    def prepareNewRound(self):
+        self.gameState = GameState()
+        self.playableFields = [0,1,2,3,4,5,6,7,8]
+
     def makeMove(self, board, field):
+        self.whoseTurnSymbol = game.playerSymbol[game.getWhoseTurn()]
         return self.gameState.makeMove(board,field)
+
     def checkLocalWin(self):
         return self.gameState.checkLocalWin()
+
     def checkGlobalWin(self):
         return self.gameState.checkGlobalWin()
+
     def getWhoseTurn(self):
         return self.gameState.getWhoseTurn()
+
     def isBoardNotPlayable(self, board):
         return self.gameState.isBoardNotPlayable(board)
+
     def getNextBoard(self):
         return self.gameState.getNextBoard()
-    
 
 game=Game()
 
 def handleClickedField(data):
-    #global wonLocalBoards
     global game
-    whoseTurn = game.playerSymbol[game.getWhoseTurn()]
     toLighten = []
     id = int(data['id'])
     board = id // 10
     field = id % 10
     correct = game.makeMove(board, field)
-    #print(correct, board, field, wonLocalBoards)
     print(correct, board, field, game.playableFields)
-    if(correct):
-        inHtml = whoseTurn
-        ######print(game.checkLocalWin())
-        ######if(game.checkLocalWin() != 0):
+    if correct:
         print(game.isBoardNotPlayable(board))
-        if(game.isBoardNotPlayable(board)):
+        if game.isBoardNotPlayable(board):
             game.playableFields.remove(board)
 
         nextBoard = game.getNextBoard()
 
-        if(nextBoard == 9):
+        if nextBoard == 9:
             toLighten = game.playableFields
         else:
             toLighten.append(nextBoard)
         
-        data['inHtml'] = inHtml
+        data['inHtml'] = game.whoseTurnSymbol
         data['toLighten'] = toLighten
-        try:
-            game.playableFields.index(board)
+        
+        if not game.checkLocalWin():
             data['localGameEnded'] = False
             data['localBoardWinner'] = ''
-        except:
+        else:
             data['localGameEnded'] = True
-            data['localBoardWinner'] = inHtml
+            data['localBoardWinner'] = game.whoseTurnSymbol
+
         print(toLighten)
         emit('actualizeView', data, broadcast=True)
 
