20b53b463618ad6f68e793bae829d6a49f403fa4
nadadrian <adrian.nadrat@wp.pl>
2019-03-29 11:44:02 +0100

    automatyczne generowanie planszy

--- src/app/static/js/app.js
+++ src/app/static/js/app.js
@@ -1,24 +1,42 @@
 var whoNow = 'X'
+var generateLocalBoard = function (id) {
+    if (id % 3 == 0) {
+        $('#globalBoard').append('<tr id="row' + parseInt(id / 3) + '"></tr>');
+    }
+    $('#row' + parseInt(id / 3)).append('<td id="' + id + '"></td>');
+    $('#' + id).append('<table id="table' + id + '" class="localBoard"></table>');
+    for (let row = 0; row < 3; ++row) {
+        $('#table' + id).append('<tr id="row' + id + row + '"></tr>')
+        for (let col = 0; col < 3; ++col) {
+            var localId = row * 3 + col;
+            $('#row' + id + row).append('<td id="' + id + localId + '" class="clue field bordered ' + id + '"></td>')
+        }
+    }
+}
 $(document).ready(function () {
     namespace = '/test';
     var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port + namespace);
-    $(".globalBoard").hide();
-    $(".welcomeInfo").show();
+
+    for (let i = 0; i < 9; ++i) {
+        generateLocalBoard(i);
+    }
+
+    $("#globalBoard").hide();
+    $("#welcomeInfo").show();
 
     socket.on('startGame', function () {
         console.log('gamestart');
-        $(".welcomeInfo").hide();
-        $(".globalBoard").show();
+        $("#welcomeInfo").hide();
+        $("#globalBoard").show();
     });
 
-    socket.on('stopGame', function() {
+    socket.on('stopGame', function () {
         console.log('gamestop');
-        $(".globalBoard").hide();
-        $(".welcomeInfo").show();
+        $("#globalBoard").hide();
+        $("#welcomeInfo").show();
     });
     for (let i = 0; i < 9; ++i) {
-        var classNameToLighten = '.' + i;
-        $(classNameToLighten).addClass('clue');
+        $('.' + i).addClass('clue');
     }
 
     socket.on('respondToReceivedMessage', function (msg) {
@@ -27,6 +45,11 @@ $(document).ready(function () {
     socket.on('actualizeView', function (data) {
         boardId = parseInt(data.id / 10);
         document.getElementById(data.id).innerHTML = data.inHtml;
+        if (data.inHtml == 'X') {
+            $('#' + data.id).addClass('takenByX');
+        } else {
+            $('#' + data.id).addClass('takenByY');
+        }
         for (let i = 0; i < 9; ++i) {
             var classNameToRemoveLighten = '.' + i;
             $(classNameToRemoveLighten).removeClass('clue');
@@ -39,6 +62,11 @@ $(document).ready(function () {
             $('.' + boardId).hide();
             $('#' + boardId).addClass('finishedLocal');
             document.getElementById(boardId).innerHTML = data.localBoardWinner;
+            if (data.localBoardWinner == 'X') {
+                $('#' + boardId).addClass('takenByX');
+            } else {
+                $('#' + boardId).addClass('takenByY');
+            }
             console.log(document.getElementById(boardId).innerHTML);
         }
     });
