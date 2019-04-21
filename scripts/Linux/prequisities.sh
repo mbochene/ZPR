b24907270371bccf6f376f23adc7a7207a431392
mbochene <mtbochenek@gmail.com>
2019-04-21 19:04:48 +0200

    Zmiana skryptów na python2.7 (ubuntu)

--- scripts/Linux/prequisities.sh
+++ scripts/Linux/prequisities.sh
@@ -1,7 +1,7 @@
 sudo apt-get install libboost-all-dev
 sudo apt-get install scons
-sudo apt install python3-pip
-pip3 install -U Flask
-pip3 install flask-socketio
-pip3 install eventlet
-pip3 install pytest
+sudo apt install python-pip
+pip install -U Flask
+pip install flask-socketio
+pip install eventlet
+pip install pytest
