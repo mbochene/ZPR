49525dbaac0220e9b0141cec98143371adb08051
nadadrian <adrian.nadrat@wp.pl>
2019-04-30 12:05:34 +0200

    Aktualizacja skryptow dla Ubuntu(pyenv)

--- scripts/Linux/prequisities.sh
+++ scripts/Linux/prequisities.sh
@@ -1,7 +1,19 @@
+#!/bin/bash
+sudo apt-get update
+sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
+if ! [ -x "$(command -v pyenv)" ]; then
+	curl https://pyenv.run | bash
+	echo "\nexport PATH=\"/home/$USER/.pyenv/bin:$PATH\"" >> ~/.bashrc
+	echo "eval \"$(pyenv init -)\"" >> ~/.bashrc
+	echo "eval \"$(pyenv virtualenv-init -)\"" >> ~/.bashrc
+	source ~/.bashrc  
+fi
+pyenv install 2.7.15
+echo "2.7.15" > ~/.pyenv/version
 sudo apt-get install libboost-all-dev
 sudo apt-get install scons
 sudo apt install python-pip
 pip install -U Flask
 pip install flask-socketio
 pip install eventlet
-pip install pytest
+sudo apt install python-pytest
--- scripts/Linux/runServer.sh
+++ scripts/Linux/runServer.sh
@@ -1,3 +1,3 @@
 py.test -v ../../src/server/tests
-#./test --log_level=test_suite >&2
-#./../../src/server/run.py
+./test --log_level=test_suite >&2
+./../../src/server/run.py
