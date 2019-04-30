21fc18da9c99898da6b3d2c1cffb931535fa1ef7
nadadrian <adrian.nadrat@wp.pl>
2019-04-30 12:50:22 +0200

    poprawka w prequisities.sh

--- scripts/Linux/prequisities.sh
+++ scripts/Linux/prequisities.sh
@@ -3,10 +3,11 @@ sudo apt-get update
 sudo apt-get install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
 if ! [ -x "$(command -v pyenv)" ]; then
 	curl https://pyenv.run | bash
-	echo "\nexport PATH=\"/home/$USER/.pyenv/bin:$PATH\"" >> ~/.bashrc
-	echo "eval \"$(pyenv init -)\"" >> ~/.bashrc
-	echo "eval \"$(pyenv virtualenv-init -)\"" >> ~/.bashrc
-	source ~/.bashrc  
+	echo "" >> ~/.bashrc
+	echo 'export PATH="~/.pyenv/bin:$PATH"' >> ~/.bashrc
+	echo 'eval "$(pyenv init -)"' >> ~/.bashrc
+	echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.bashrc
+	exec bash
 fi
 pyenv install 2.7.15
 echo "2.7.15" > ~/.pyenv/version
