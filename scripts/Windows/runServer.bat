0b7aa8d606ba2c24b8f8935b4ea289374557c5af
mbochene <38912172+mbochene@users.noreply.github.com>
2019-05-17 22:13:09 +0200

    Poprawka skryptu runServer.bat

--- scripts/Windows/runServer.bat
+++ scripts/Windows/runServer.bat
@@ -4,6 +4,6 @@ cd ../..
 Call venv\Scripts\activate
 py.test -v src/server/tests
 %cd%\src\server\engine\tests\test.exe --log_level=test_suite
-py -3 src/server/run.py
+Call py -3 src/server/run.py
 Call deactivate
 cd %currDir%
